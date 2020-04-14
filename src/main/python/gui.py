b3e32b4480fe240b38e96c23c5d43bd24a6097ed
Edward Fernandez <edward@flashnetsolutions.com>
2020-04-14 12:50:40 -0400

    First commit

---
+++ src/main/python/gui.py
@@ -0,0 +1,13 @@
+# import widgets: buttons, labels, etc
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPalette
+app = QApplication([])
+palette = QPalette()
+#TODO: Set palette colors here!
+
+#TODO: set stylesheets
+# app.setStyleSheet("QPushButton { margin: 10ex; }")
+window = QWidget()
+# Set overall layout
+main_layout
